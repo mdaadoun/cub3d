15ad77c4c7130d8772fcc72a59ea8d2d224cbc47
Dylan Laidet <dlaidet@1A7.42angouleme.fr>
2022-09-22 09:40:12 +0200

    Window open and can be closed by 'Esc' and red cross

--- src/cub_main.c
+++ src/cub_main.c
@@ -6,7 +6,7 @@
 /*   By: mdaadoun <mdaadoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 12:16:31 by dlaidet           #+#    #+#             */
-/*   Updated: 2022/09/22 09:17:34 by dlaidet          ###   ########.fr       */
+/*   Updated: 2022/09/22 09:38:21 by dlaidet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,19 @@
 
 // }
 
+int	fs_close(t_cub *cub)
+{
+	cub_free_before_exit(cub, NO_ERROR);
+	return (0);
+}
+
+int	fs_key_hook(int keycode, t_cub *cub)
+{
+	if (keycode == 65307)
+		cub_free_before_exit(cub, NO_ERROR);
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	t_cub	cub;
@@ -26,8 +39,11 @@ int	main(int ac, char **av)
 	ft_printf("Cub3D running...\n");
 //	cub_data_set(&cub, ac, av);
 	cub_init(&cub);
+	mlx_key_hook(cub.win.win, fs_key_hook, &cub);
+	mlx_hook(cub.win.win, DestroyNotify, 1L << 17, fs_close, &cub);
+	mlx_loop(cub.win.mlx);
 	cub_free_before_exit(&cub, NO_ERROR);
 	(void)ac;
-(void)av;
+	(void)av;
 	return (0);
 }
