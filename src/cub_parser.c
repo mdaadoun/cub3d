edade08b99b03cbf96dc81cd27049c8b28611584
Dylan Laidet <dlaidet@1A7.42angouleme.fr>
2022-09-24 08:01:07 +0200

    No leaks anymore but don't take the multi set same data

--- src/cub_parser.c
+++ src/cub_parser.c
@@ -6,20 +6,24 @@
 /*   By: mdaadoun <mdaadoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 09:23:04 by mdaadoun          #+#    #+#             */
-/*   Updated: 2022/09/23 15:31:00 by mdaadoun         ###   ########.fr       */
+/*   Updated: 2022/09/24 07:54:53 by dlaidet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub.h"
 
-static char	**fs_split_and_trim(t_cub *cub, char *line)
+static char	*fs_split_and_trim(t_cub *cub, char *line)
 {
 	char	*trim;
 	char	**split;
+	char	*ret;
 
 	trim = ft_strtrim(line, " \n");
 	if (ft_strlen(trim) == 0)
+	{
+		free(trim);
 		return (NULL);
+	}
 	split = ft_split(trim, ' ');
 	free(trim);
 	if (split[2])
@@ -27,35 +31,36 @@ static char	**fs_split_and_trim(t_cub *cub, char *line)
 		cub_free_split(split);
 		cub_free_before_exit(cub, ERROR_FORMAT);
 	}
-	return (split);
+	ret = split[1];
+	free(split[0]);
+	free(split);
+	return (ret);
 }
 
 static t_u8	fs_save_data(t_cub *cub, char *line, t_u8 count)
 {
-	char		**split;
 	t_data		*data;
+	size_t		len;
 
 	data = cub->data;
 	count++;
-	if (!ft_char_in_set(line[0], "\nFENSWC"))
-		cub_free_before_exit(cub, ERROR_FORMAT);
-	split = fs_split_and_trim(cub, line);
-	if (line[0] == 'F' && line[1] == ' ')
-		data->color_F = split[1];
-	else if (line[0] == 'C' && line[1] == ' ')
-		data->color_C = split[1];
-	else if (line[0] == 'E' && line[1] == 'A' && line[2] == ' ')
-		data->imgpath_EA = split[1];
-	else if (line[0] == 'N' && line[1] == 'O' && line[2] == ' ')
-		data->imgpath_NO = split[1];
-	else if (line[0] == 'S' && line[1] == 'O' && line[2] == ' ')
-		data->imgpath_SO = split[1];
-	else if (line[0] == 'W' && line[1] == 'E' && line[2] == ' ')
-		data->imgpath_WE = split[1];
-	else
+	len = ft_strlen(line);
+	if (line[0] == '\n')
 		count--;
-	// if (split)
-	// 	cub_free_split(split);
+	else if (len >= 2 && line[0] == 'F' && line[1] == ' ')
+		data->color_F = fs_split_and_trim(cub, line);
+	else if (len >= 2 && line[0] == 'C' && line[1] == ' ')
+		data->color_C = fs_split_and_trim(cub, line);
+	else if (len >= 3 && line[0] == 'E' && line[1] == 'A' && line[2] == ' ')
+		data->imgpath_EA = fs_split_and_trim(cub, line);
+	else if (len >= 3 && line[0] == 'N' && line[1] == 'O' && line[2] == ' ')
+		data->imgpath_NO = fs_split_and_trim(cub, line);
+	else if (len >= 3 && line[0] == 'S' && line[1] == 'O' && line[2] == ' ')
+		data->imgpath_SO = fs_split_and_trim(cub, line);
+	else if (len >= 3 && line[0] == 'W' && line[1] == 'E' && line[2] == ' ')
+		data->imgpath_WE = fs_split_and_trim(cub, line);
+	else
+		cub_free_before_exit(cub, ERROR_FORMAT);
 	return (count);
 }
 
