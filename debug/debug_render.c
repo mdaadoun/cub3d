c66e1203c8b8194fc50a4c971c04dc17d6935bd2
Dylan Laidet <dlaidet@1A7.42angouleme.fr>
2022-10-12 10:17:28 +0200

    Debug render rework

--- debug/debug_render.c
+++ debug/debug_render.c
@@ -39,20 +39,47 @@ void	dg_get_col_img(t_cub *cub, int col, t_ray *ray)
 	t_buffer	*bs;
 	char		*pix;
 	int			y;
+	t_f64		size;
+	int			lineheight;
+	int			drawstart;
+	int			drawend;
+	int			a;
+	int			b;
+	int			c;
 
+	lineheight = (int)(HEIGHT / ray->length) * 64;
+	drawstart = -lineheight / 2 + HEIGHT / 2;
+	if (drawstart < 0)
+		drawstart = 0;
+	drawend = lineheight / 2 + HEIGHT / 2;
+	if (drawend >= HEIGHT)
+		drawend = HEIGHT - 1;
+	printf("Lineheight: %d	DrawStart: %d	DrawEnd: %d\n",lineheight, drawstart, drawend);
+	size = lineheight / 64;
+	if (size == 0)
+		size = 1;
+	printf("Size: %f\n", size);
 	y = 0;
 	bs = cub_alloc(cub, sizeof(t_buffer), 1);
 	bs->buffer = mlx_get_data_addr(cub->display->img, \
 			&bs->pixel_bits, &bs->line_bytes, &bs->endian);
-	while (y < 64)
+	a = size * 64;
+	if (a > HEIGHT)
 	{
-		pix = dg_get_pixel(bs, col, ((y * 2) + HEIGHT) / 2);
+		b = a - HEIGHT;
+		y = (b / 2) / size;
+		c = y;
+	}
+	printf("Y: %d	64 - C:%d\n", y, 64 - c);
+	while (y < 63 - c)
+	{
+		pix = dg_get_pixel(bs, col, drawstart + y);
 		if (ray->texture == TEXTURE_NORTH || ray->texture == TEXTURE_SOUTH)
 			dg_copy_pixel(pix, dg_get_pixel(dg_get_texture(cub, ray->texture), \
-					ray->target_x % 64, y), 2);
+					ray->target_x % 64, y), size);
 		else
 			dg_copy_pixel(pix, dg_get_pixel(dg_get_texture(cub, ray->texture), \
-					ray->target_y % 64, y), 2);
+					ray->target_y % 64, y), size);
 		y++;
 	}
 	free(bs);
